837627ab83e1b51dd1a9e39fea230742f52e317f
Michael Cabral <akakubiyaro@gmail.com>
2023-11-06 00:00:04 -0600

    assets

--- src/visuals/Assets.h
+++ src/visuals/Assets.h
@@ -14,3 +14,15 @@
 
 #define TILE_W 32
 #define TILE_H 32
+
+#define GLYPH_W 16
+#define GLYPH_H 16
+
+struct Assets
+{
+	TileSet figureTiles;
+	TileSet mapTiles;
+	TileSet glyphTiles;
+};
+
+extern struct Assets Assets;
